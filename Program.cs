ba9c67ffcdbcfa37dd56d266b3ff85b10990e6c0
intisor <abdulawwalintisor777@gmail.com>
2026-06-07 14:12:24 +0100

    refactor: move source files out of src folder directly into root project directory

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TurnIt;
 using TurnIt.Services;
-using TurnIt.src;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
